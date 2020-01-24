8119f39ac7171f37ad50e12760339642d49ea334
Nas1edNIK <Nikok@zos-v.ru>
2020-01-24 14:34:53 +0400

    add new project

---
+++ Patterns/PatternsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace Patterns
+{
+    class PatternsModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Reference { get; set; }
+        public string Example { get; set; }
+        public string ImageSource { get; set; }
+        public object Diagram { get; set; }
+    }
+}
---
+++ Patterns/PatternsTreeView.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Navigation;
+
+namespace Patterns
+{
+    /// <summary>
+    /// Interaction logic for PatternsTreeView.xaml
+    /// </summary>
+    public partial class PatternsTreeView : UserControl
+    {
+        public PatternsTreeView()
+        {
+            InitializeComponent();
+            DataContext = new PatternsTreeViewModel();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+
+            e.Handled = true;
+        }
+
+        private void ScrollViewer_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            var sv = (sender as ScrollViewer);
+
+            if(e.Delta > 0)
+                sv.LineUp();
+            else
+                sv.LineDown();
+        }
+    }
+}
